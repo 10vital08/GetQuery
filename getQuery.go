9656069c96b6f6227a52ef3524850cf2f73af94d
10vital08 <vital.lushnikov@mail.ru>
2021-10-04 22:18:33 +0300

    Update getQuery.go

--- getQuery.go
+++ getQuery.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"log"
-	"net/http"
-	"io"
+	"net"
 )
 
-func handler(w http.ResponseWriter, r *http.Request)  {
-	fmt.Println(fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path))
+
+/*func handler(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-}
+	http.HandleFunc("/", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	//fmt.Println("1")
+}*/
