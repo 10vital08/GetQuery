102578a48940abfcb8cc24debf7795ea9f33f1a8
10vital08 <vital.lushnikov@mail.ru>
2021-10-04 21:06:05 +0300

    Update getQuery.go

--- getQuery.go
+++ getQuery.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"log"
+	"net/http"
+	"io"
+)
 
+func handler(w http.ResponseWriter, r *http.Request)  {
+	fmt.Println(fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path))
+}
+
+func main() {
+	http.HandleFunc("/", handler)
 }
