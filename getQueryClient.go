8c0a970ede24ff1b45d73383d50c95fb1ae8677f
10vital08 <vital.lushnikov@mail.ru>
2021-10-13 22:16:48 +0300

    Horror

--- getQueryClient.go
+++ getQueryClient.go
@@ -2,31 +2,37 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 var query string
 
-func makeQuery(w http.ResponseWriter, r *http.Request, query string)  {
-	fmt.Fprintf(w, "Query")
-	w.Write([]byte(q))
-}
+func makeQuery(w http.ResponseWriter, r *http.Request)  {
+	//fmt.Fprintf(w, "Query")
+	//w.Write([]byte(query))
 
-func returnAnswer()  {
-	
+	responseData, err := ioutil.ReadAll(r.Body)
+	if err!= nil{
+		log.Fatal(err)
+	}
+
+	responseString := string(responseData)
+	fmt.Println(responseString)
 }
 
 func main(){
-	query = "123"
-
-	vebServer := http.NewServeMux()
+	query = "test"
+	//vebServer := http.NewServeMux()
 	http.HandleFunc("/", makeQuery)//установка роутера
 	log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
-	err := http.ListenAndServe(":8080", vebServer)//установка порта
+	err := http.ListenAndServe(":8080", nil)//установка порта
 	log.Fatal(err)
 
-	resp, err := http.Get("http://127.0.0.1:8080", query)
+	resp, err := http.Get("http://127.0.0.1:8080?query=" + query)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer resp.Body.Close()
-
 }
--- getQueryServer.go
+++ getQueryServer.go
@@ -2,43 +2,40 @@ package main
 
 import (
 	"fmt"
+	//"io/ioutil"
 	"log"
 	"net/http"
+	//"strconv"
 )
-	var q string
+
+var query string
 
 func makeQuery(w http.ResponseWriter, r *http.Request)  {
-	w.Write([]byte(q))
+	/*id, err := strconv.Atoi(r.URL.Query().Get("query"))
+	responseString := string(id)
+	fmt.Fprintf(w, responseString)*/
+	
+	query2 := r.URL.Query().Get("query")
+	responseString := string(query2)
+	fmt.Fprintf(w, responseString)
+}
 
-	//fmt.Fprintf(w,"Test")
+func createQuery (w http.ResponseWriter, r *http.Request)  {
+	resp, err := http.Get("http://127.0.0.1:8080?query=test" + query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
 }
 
 func main(){
-	q = "test"
-	http.HandleFunc("/", makeQuery)//установка роутера
-	log.Println("http://127.0.0.1:8080")
-	err := http.ListenAndServe(":8080", nil)//установка порта
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	query = "test"
+	vebServer := http.NewServeMux()
+	vebServer.HandleFunc("/", makeQuery)//установка роутера
+	log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
+	err := http.ListenAndServe(":8080", vebServer)//установка порта
+	log.Fatal(err)
 
-	resp, err := http.Get("http://127.0.0.1:8080")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(resp)
-	defer resp.Body.Close()
-
-	/*for true {
-
-		bs := make([]byte, 1014)
-		n, err := resp.Body.Read(bs)
-		fmt.Println(string(bs[:n]))
-
-		if n == 0 || err != nil{
-			break
-		}
-	}*/
-
+	vebServer.HandleFunc("/", createQuery)
+	vebServer.HandleFunc("/", makeQuery)
 }
