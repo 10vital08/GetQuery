00d0a220fb3e9daf9557dd5ee415d2b1125e9a29
10vital08 <vital.lushnikov@mail.ru>
2021-10-27 22:01:14 +0300

    VS Code

--- getQueryServer.go
+++ getQueryServer.go
@@ -2,21 +2,43 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 var query string
 
-func handleQuery(w http.ResponseWriter, r *http.Request)  {
+func handleQuery(w http.ResponseWriter, r *http.Request) {
 	query := string(r.URL.Query().Get("query"))
 	fmt.Fprintf(w, query)
 }
 
-func main(){
+func handleHtmlPage(w http.ResponseWriter, r *http.Request) {
+	path := filepath.Join("public", "html", "page.html")
+
+	//создаем html-шаблон
+	tmpl, err := template.ParseFiles(path)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	//выводим шаблон клиенту в браузер
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+}
+
+func main() {
 	webServer := http.NewServeMux()
 	webServer.HandleFunc("/", handleQuery) // установка роутера
+	webServer.HandleFunc("/html", handleHtmlPage)
 	log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
 	err := http.ListenAndServe(":8080", webServer) // установка порта
 	log.Fatal(err)
+
 }
